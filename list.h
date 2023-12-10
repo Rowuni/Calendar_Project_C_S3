d9598b23ee69e04f82f05df5727f68be2e8683fc
Rowuni <125250963+Rowuni@users.noreply.github.com>
2023-12-10 22:42:48 +0100

    V 1.0

--- list.h
+++ list.h
@@ -9,23 +9,27 @@
 
 typedef struct s_d_cell
 {
-    int value;
-    struct s_d_cell **next; // array of next pointers
+    int value; // Niveau de la cellule
+    struct s_d_cell **next; // Tableau de pointeurs vers les cellules suivantes
 } t_d_cell; 
 
 typedef struct s_d_list
 {
-    t_d_cell **head; // array of head pointers
-    int level_max; // height of the list
-    int size; // number of elements in the list
+    t_d_cell **head; // Tableau de pointeurs vers les cellules de la liste
+    int level_max; // Hauteur maximale de la liste
+    int size; // Nombre de cellules dans la liste
 } t_d_list; 
+
+
+// Définitions des fonctions
 
 t_d_list *create_d_list(int level_max);
 t_d_cell *create_d_cell(int value, int level_max);
-void insert_d_list(t_d_list *list, int value);
-void delete_d_list(t_d_list *list, int value);
+void insert_d_list(t_d_list *list, int value, int level);
 t_d_cell *search_d_list(t_d_list *list, int value);
+void delete_d_list(t_d_list *list, int value);
 void print_d_list(t_d_list *list);
 void free_d_list(t_d_list *list);
+void insert_head_d_list(t_d_list *list, int value, int level);
 
 #endif
